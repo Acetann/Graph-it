813e3ef95af7413708a2086ddc87faca73ec6762
Fremont Paul <paul.fremont@viacesi.fr>
2020-09-07 14:07:42 +0200

    Accueil + Blog

---
+++ screens/Accueil.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export default function Accueil(){
+
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    }
+)
