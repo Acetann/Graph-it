0ccf46ced1559bdacdf5422754e31a234b83670d
randazzo-pierre <58936117+randazzo-pierre@users.noreply.github.com>
2020-09-08 15:39:02 +0200

    data real + services

--- screens/Realisation.js
+++ screens/Realisation.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, StyleSheet, Text ,Image, ScrollView} from 'react-native';
 
 
 export default function Realisation(){
 
+    const [data,setData] = useState(null)
+
+    function getRealisations(){
+        fetch('http://graph-it.cesi.group/realisations', {
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) =>
+            {
+                response.json()
+                    .then((responseData) =>
+                    {
+                        console.log(responseData)
+                        setData(responseData)
+                    });
+            })
+            .catch(function(err)
+            {
+                console.log(err)
+            })
+    }
+    var ViewContent = [];
+
+    if(data != null){
+        for(var i = 0; i<data.length;i++){
+            ViewContent.push(
+
+                <View style={styles.container}>
+                    <Text style={styles.realisationTitre}> {data[i].TitreR}</Text>
+                </View>
+
+            )
+        }
+    }
+    else{
+        getRealisations();
+    }
 
     return (
         <View>
-            <Text>Nos réalisations</Text>
+
+            <ScrollView>
+                {ViewContent}
+            </ScrollView>
         </View>
     )
 }
@@ -18,5 +62,21 @@ const styles = StyleSheet.create({
             justifyContent : 'center',
             alignItems: 'center',
     },
+
+    realisationImage:{
+        marginTop:5,
+        marginBottom:2,
+        justifyContent:'center',
+        width:150,
+        height:150,
+        borderRadius:100,
+    },
+
+    realisationTitre:{
+        fontWeight:'600',
+        fontSize:20,
+        fontFamily: 'sans-serif',
+        alignItems:'center',
+    }
     }
 )
--- screens/Service.js
+++ screens/Service.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, StyleSheet, Text ,Image, ScrollView} from 'react-native';
 
 export default function Service(){
 
+    const [data,setData] = useState(null)
+    
+    function getServices(){
+        fetch('http://graph-it.cesi.group/services', {
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) =>
+            {
+                response.json()
+                    .then((responseData) =>
+                    {
+                        console.log(responseData)
+                        setData(responseData)
+                    });
+            })
+            .catch(function(err)
+            {
+                console.log(err)
+            })
+    }
+    var ViewContent = [];
+
+    if(data != null){
+        for(var i = 0; i<data.length;i++){
+            ViewContent.push(
+
+                <View style={styles.container}>
+                    <Text style={styles.ServiceTitre}> {data[i].TitreS}</Text>
+                </View>
+
+            )
+        }
+    }
+    else{
+        getServices();
+    }
+
     return (
         <View>
-
+            <ScrollView>
+                {ViewContent}
+            </ScrollView>
         </View>
     )
 }
@@ -16,5 +60,21 @@ const styles = StyleSheet.create({
             justifyContent : 'center',
             alignItems: 'center',
     },
+
+    realisationImage:{
+        marginTop:5,
+        marginBottom:2,
+        justifyContent:'center',
+        width:150,
+        height:150,
+        borderRadius:100,
+    },
+
+    realisationTitre:{
+        fontWeight:'600',
+        fontSize:20,
+        fontFamily: 'sans-serif',
+        alignItems:'center',
+    }
     }
 )
