09bc6d6808f771012f08e1443f0beca645da1141
Fremont Paul <paul.fremont@viacesi.fr>
2020-09-08 11:22:53 +0200

    Barre en bas

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 
 import MyHeader from './components/Header';
 
+
 import LoadingScreen from './screens/loadingScreen';
 import Accueil from './screens/Accueil';
 import Blog from './screens/Blog';
@@ -27,7 +28,23 @@ export default function App() {
     <SafeAreaView style={styles.container}>
 
       <MyHeader title={page} />
+      <View style={{ flex: 1 }}>
+        {page === 'Accueil' &&
+          <Accueil navigate={navigate} />}
 
+        {page === 'Blog' && <Blog />}
+
+        {page === 'Contact' && <Contact />}
+
+        {page === 'Equipe' && <Equipe />}
+
+        {page === 'Realisation' && <Realisation />}
+
+        {page === 'Service' && <Service />}
+
+        {page === 'LoadingScreen' && <LoadingScreen />}
+
+      </View>
       <View style={styles.menu}>
 
         <Button
@@ -68,21 +85,6 @@ export default function App() {
 
       </View>
 
-      {page === 'Accueil' &&
-        <Accueil navigate={navigate} />}
-
-      {page === 'Blog' && <Blog />}
-
-      {page === 'Contact' && <Contact />}
-
-      {page === 'Equipe' && <Equipe />}
-
-      {page === 'Realisation' && <Realisation />}
-
-      {page === 'Service' && <Service />}
-
-      {page === 'LoadingScreen' && <LoadingScreen />}
-
     </SafeAreaView>
 
   );
@@ -90,15 +92,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
 
-  menu: {
-
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
+
+  menu: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+
+  },
+
+
 });
