7345f851482246bff1d239627f69da5e987dcad5
randazzo-pierre <58936117+randazzo-pierre@users.noreply.github.com>
2020-09-11 17:20:48 +0200

    correct

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { Header } from 'react-native-elements';
 
 export default function MyHeader({ title }) {
 
-    const img = 'https://raw.githubusercontent.com/Noe-Louvell/graph-it_front/master/src/assets/img/bg-page2.png?token=AN7XEMQFMBPRHMHZEH2LWG27LHKUS'
+    const img = 'http://graph-it.cesi.group/uploads/bg_page2_08c2e9f9a8.png'
     return (
         <Header
             backgroundImage={{ uri: img }}
@@ -14,7 +14,7 @@ export default function MyHeader({ title }) {
             rightComponent={{ ImageBackground: 'https://raw.githubusercontent.com/Noe-Louvell/graph-it_front/master/src/assets/img/logo_1.png?token=AN7XEMT5ELUWHUCKAUXSKI27LHK7A', width:'100%', height:20, color: '#fff' }}
         />
     )
-    
+
 }
 
 const styles = StyleSheet.create({
